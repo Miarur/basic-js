f5408c934d56cc50f8f29f3206d35418635ea4f4
miarur <deadheartedfo@yahoo.com>
2022-05-01 20:36:24 +0300

    feat: done carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+
+  if(!(typeof sampleActivity === 'string')) return false; 
+
+  const HALF_LIFE_PERIOD = 5730; 
+  const MODERN_ACTIVITY = 15; 
+  const SAMPLE_ACTIVITY = +sampleActivity > 0 && sampleActivity <= 15 ? +sampleActivity : 0; 
+
+  const k =  ( 0.693 / HALF_LIFE_PERIOD);
+  console.log(k);
+
+  let time = Math.ceil(Math.log(MODERN_ACTIVITY/ SAMPLE_ACTIVITY) / k);
+  console.log(time);
+  return time === Infinity ? false : time; 
 }
 
 module.exports = {
